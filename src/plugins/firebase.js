5f1ebf5cb845c5a5dfbfecc4f0b2ff735c806876
Syromiatnikov Vladimir <syromiatnikov.v@gmail.com>
2020-08-10 20:49:17 +0300

    adding firebase

---
+++ src/plugins/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDPQVce3W3U0dB4mTbxTxfdY4WB25C3mf0',
+  authDomain: 'user-5a231.firebaseapp.com',
+  databaseURL: 'https://user-5a231.firebaseio.com',
+  projectId: 'user-5a231',
+  storageBucket: 'user-5a231.appspot.com',
+  messagingSenderId: '690171817864',
+  appId: '1:690171817864:web:796c11c935eb40d090143d',
+}
+
+firebase.initializeApp(firebaseConfig)
